236fa9dab43d8ecf8923e137f0b4fb6247822c7b
Vedant-N421 <147396311+Vedant-N421@users.noreply.github.com>
2024-05-16 14:24:43 +0100

    MTDSA-23435: Adds necessary files and new routes for v2.

---
+++ app/v2/connectors/OtherEmploymentIncomeConnector.scala
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.connectors
+
+import api.connectors.DownstreamUri.{DesUri, IfsUri, TaxYearSpecificIfsUri}
+import api.connectors.{BaseDownstreamConnector, DownstreamOutcome}
+import config.{AppConfig, FeatureSwitches}
+import play.api.http.Status.NO_CONTENT
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
+import v2.models.request.otherEmploymentIncome.OtherEmploymentIncomeRequest
+import v2.models.response.retrieveOtherEmployment.RetrieveOtherEmploymentResponse
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class OtherEmploymentIncomeConnector @Inject()(val http: HttpClient, val appConfig: AppConfig)(implicit featureSwitches: FeatureSwitches)
+    extends BaseDownstreamConnector {
+
+  def deleteOtherEmploymentIncome(request: OtherEmploymentIncomeRequest)(implicit
+      hc: HeaderCarrier,
+      ec: ExecutionContext,
+      correlationId: String): Future[DownstreamOutcome[Unit]] = {
+
+    import api.connectors.httpparsers.StandardDownstreamHttpParser._
+
+    implicit val successCode: SuccessCode = SuccessCode(NO_CONTENT)
+
+    val path = s"income-tax/income/other/employments/${request.nino}/${request.taxYear.asMtd}"
+
+    val downstreamUri =
+      if (request.taxYear.useTaxYearSpecificApi) {
+        TaxYearSpecificIfsUri[Unit](s"income-tax/income/other/employments/${request.taxYear.asTysDownstream}/${request.nino}")
+      } else if (featureSwitches.isDesIf_MigrationEnabled) {
+        IfsUri[Unit](path)
+      } else {
+        DesUri[Unit](path)
+      }
+
+    delete(
+      uri = downstreamUri
+    )
+  }
+
+  def retrieveOtherEmploymentIncome(request: OtherEmploymentIncomeRequest)(implicit
+      hc: HeaderCarrier,
+      ec: ExecutionContext,
+      correlationId: String): Future[DownstreamOutcome[RetrieveOtherEmploymentResponse]] = {
+
+    import api.connectors.httpparsers.StandardDownstreamHttpParser._
+
+    val path = s"income-tax/income/other/employments/${request.nino}/${request.taxYear.asMtd}"
+
+    val resolvedDownstreamUri = if (request.taxYear.useTaxYearSpecificApi) {
+      TaxYearSpecificIfsUri[RetrieveOtherEmploymentResponse](
+        s"income-tax/income/other/employments/${request.taxYear.asTysDownstream}/${request.nino}")
+    } else if (featureSwitches.isDesIf_MigrationEnabled) {
+      IfsUri[RetrieveOtherEmploymentResponse](path)
+    } else {
+      DesUri[RetrieveOtherEmploymentResponse](path)
+    }
+
+    get(uri = resolvedDownstreamUri)
+  }
+
+}
---
+++ app/v2/controllers/RetrieveOtherEmploymentController.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.controllers
+
+import api.controllers.{AuthorisedController, EndpointLogContext, RequestContext, RequestHandler}
+import api.services.{EnrolmentsAuthService, MtdIdLookupService}
+import config.AppConfig
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import utils.IdGenerator
+import v2.controllers.requestParsers.OtherEmploymentIncomeRequestParser
+import v2.models.request.otherEmploymentIncome.OtherEmploymentIncomeRequestRawData
+import v2.services.RetrieveOtherEmploymentIncomeService
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class RetrieveOtherEmploymentController @Inject()(val authService: EnrolmentsAuthService,
+                                                  val lookupService: MtdIdLookupService,
+                                                  parser: OtherEmploymentIncomeRequestParser,
+                                                  service: RetrieveOtherEmploymentIncomeService,
+                                                  cc: ControllerComponents,
+                                                  val idGenerator: IdGenerator)(implicit ec: ExecutionContext, appConfig: AppConfig)
+    extends AuthorisedController(cc) {
+
+  implicit val endpointLogContext: EndpointLogContext =
+    EndpointLogContext(
+      controllerName = "RetrieveOtherEmploymentController",
+      endpointName = "retrieveOtherEmployment"
+    )
+
+  def retrieveOther(nino: String, taxYear: String): Action[AnyContent] =
+    authorisedAction(nino).async { implicit request =>
+      implicit val ctx: RequestContext = RequestContext.from(idGenerator, endpointLogContext)
+
+      val rawData: OtherEmploymentIncomeRequestRawData = OtherEmploymentIncomeRequestRawData(
+        nino = nino,
+        taxYear = taxYear
+      )
+
+      val requestHandler =
+        RequestHandler
+          .withParser(parser)
+          .withService(service.retrieve)
+          .withPlainJsonResult()
+
+      requestHandler.handleRequest(rawData)
+    }
+
+}
