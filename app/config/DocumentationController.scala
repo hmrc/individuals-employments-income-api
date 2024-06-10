6df70b169c206ca045f6cfc59abacf53c04a5481
Chandra Korivi <14151747+sudoku007@users.noreply.github.com>
2024-06-10 23:01:51 +0100

    [MTDSA-24261] Use CORSActionBuilder instead of play CORS filters to preview OAS docs locally.

--- app/config/DocumentationController.scala
+++ app/config/DocumentationController.scala
@@ -18,22 +18,26 @@ package config
 
 import controllers.Assets
 import definition.ApiDefinitionFactory
+import org.apache.pekko.stream.Materializer
+import play.api.Configuration
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import play.filters.cors.CORSActionBuilder
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
 import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
 
 @Singleton
-class DocumentationController @Inject() (selfAssessmentApiDefinition: ApiDefinitionFactory, cc: ControllerComponents, assets: Assets)
-  extends BackendController(cc) {
+class DocumentationController @Inject() (selfAssessmentApiDefinition: ApiDefinitionFactory, cc: ControllerComponents, assets: Assets, configuration: Configuration)
+                                        (implicit mat: Materializer, ec: ExecutionContext) extends BackendController(cc) {
 
   def definition(): Action[AnyContent] = Action {
     Ok(Json.toJson(selfAssessmentApiDefinition.definition))
   }
 
-  def asset(version: String, file: String): Action[AnyContent] = {
-    assets.at(s"/public/api/conf/$version", file)
+  def asset(version: String, file: String): Action[AnyContent] = CORSActionBuilder(configuration).async { implicit request =>
+    assets.at(s"/public/api/conf/$version", file)(request)
   }
 
 }
