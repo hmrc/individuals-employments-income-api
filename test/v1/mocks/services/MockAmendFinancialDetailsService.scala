a255e2788e9621f0a13e209a5586d3075582461c
Vedant-N421 <147396311+Vedant-N421@users.noreply.github.com>
2024-09-09 13:46:12 +0100

    MTDSA-25548: Replaces api requestcontext with shared version

--- test/v1/mocks/services/MockAmendFinancialDetailsService.scala
+++ test/v1/mocks/services/MockAmendFinancialDetailsService.scala
@@ -16,7 +16,7 @@
 
 package v1.mocks.services
 
-import api.controllers.RequestContext
+import shared.controllers.RequestContext
 import api.services.ServiceOutcome
 import org.scalamock.handlers.CallHandler
 import org.scalamock.scalatest.MockFactory
