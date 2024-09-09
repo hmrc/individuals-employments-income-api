39a7b6b653d4b3865fd5a44251861fb2208eb5e5
Vedant-N421 <147396311+Vedant-N421@users.noreply.github.com>
2024-09-09 12:17:30 +0100

    MTDSA-25548: Fix the mock employments app config to how it should be!

--- test/mocks/MockEmploymentsAppConfig.scala
+++ test/mocks/MockEmploymentsAppConfig.scala
@@ -17,93 +17,18 @@
 package mocks
 
 import config.EmploymentsAppConfig
+import org.scalamock.handlers.CallHandler0
 import org.scalamock.scalatest.MockFactory
 import shared.config.{DownstreamConfig, MockAppConfig}
 import shared.models.domain.TaxYear
-import uk.gov.hmrc.http.HeaderCarrier
 
-trait MockEmploymentsAppConfig extends MockFactory {
+trait MockEmploymentsAppConfig extends MockFactory with MockAppConfig {
   val mockEmploymentsConfig: EmploymentsAppConfig = mock[EmploymentsAppConfig]
 
-  object MockEmploymentsAppConfig extends MockAppConfig {
-    val minimumPermittedTaxYear: TaxYear = mockEmploymentsConfig.minimumPermittedTaxYear
-    lazy val release6DownstreamConfig: DownstreamConfig = mockEmploymentsConfig.release6DownstreamConfig
-    lazy val api1661DownstreamConfig: DownstreamConfig = mockEmploymentsConfig.api1661DownstreamConfig
-
-    implicit val correlationId: String = "a1e8057e-fbbc-47a8-a8b4-78d9f015c253"
-
-    val otherHeaders: Seq[(String, String)] = List(
-      "Gov-Test-Scenario" -> "DEFAULT",
-      "AnotherHeader" -> "HeaderValue"
-    )
-
-    val dummyDesHeaderCarrierConfig: HeaderCarrier.Config =
-      HeaderCarrier.Config(
-        Seq("^not-test-BaseUrl?$".r),
-        Seq.empty[String],
-        Some("individuals-employments-income-api")
-      )
-
-    val dummyIfsHeaderCarrierConfig: HeaderCarrier.Config =
-      HeaderCarrier.Config(
-        Seq("^not-test-BaseUrl?$".r),
-        Seq.empty[String],
-        Some("individuals-employments-income-api")
-      )
-
-    val dummyHeaderCarrierConfig: HeaderCarrier.Config =
-      HeaderCarrier.Config(
-        Seq("^not-test-BaseUrl?$".r),
-        Seq.empty[String],
-        Some("individuals-employments-income-api")
-      )
-
-    val allowedDesHeaders: Seq[String] = List(
-      "Accept",
-      "Gov-Test-Scenario",
-      "Content-Type",
-      "Location",
-      "X-Request-Timestamp",
-      "X-Session-Id"
-    )
-
-    val allowedIfsHeaders: Seq[String] = List(
-      "Accept",
-      "Gov-Test-Scenario",
-      "Content-Type",
-      "Location",
-      "X-Request-Timestamp",
-      "X-Session-Id"
-    )
-
-    val requiredDesHeaders: Seq[(String, String)] = List(
-      "Environment" -> "des-environment",
-      "Authorization" -> s"Bearer des-token",
-      "CorrelationId" -> s"$correlationId"
-    )
-
-    val requiredIfsHeaders: Seq[(String, String)] = List(
-      "Environment" -> "ifs-environment",
-      "Authorization" -> s"Bearer ifs-token",
-      "CorrelationId" -> s"$correlationId"
-    )
-
-    val requiredTysIfsHeaders: Seq[(String, String)] = List(
-      "Environment" -> "TYS-IFS-environment",
-      "Authorization" -> s"Bearer TYS-IFS-token",
-      "CorrelationId" -> s"$correlationId"
-    )
-
-    val requiredRelease6Headers: Seq[(String, String)] = List(
-      "Environment" -> "release6-environment",
-      "Authorization" -> s"Bearer release6-token",
-      "CorrelationId" -> s"$correlationId"
-    )
-
-    val requiredApi1661Headers: Seq[(String, String)] = List(
-      "Environment" -> "api1661-environment",
-      "Authorization" -> s"Bearer api1661-token",
-      "CorrelationId" -> s"$correlationId"
-    )
+  object MockedEmploymentsAppConfig {
+    def minimumPermittedTaxYear: CallHandler0[TaxYear] = (() => mockEmploymentsConfig.minimumPermittedTaxYear: TaxYear).expects()
+    def release6DownstreamConfig: CallHandler0[DownstreamConfig]    = (() => mockEmploymentsConfig.release6DownstreamConfig: DownstreamConfig).expects()
+    def api1661DownstreamConfig: CallHandler0[DownstreamConfig]    = (() => mockEmploymentsConfig.api1661DownstreamConfig: DownstreamConfig).expects()
   }
+
 }
