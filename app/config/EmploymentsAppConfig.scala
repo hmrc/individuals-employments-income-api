27d28d5a6bc58ba9a5ec438b5e7e3b4adf1c228b
Thepworth95 <56401451+Thepworth95@users.noreply.github.com>
2025-07-16 11:22:46 +0100

    Delete student loans validator

--- app/config/EmploymentsAppConfig.scala
+++ app/config/EmploymentsAppConfig.scala
@@ -31,6 +31,8 @@ class EmploymentsAppConfig @Inject() (protected val config: ServicesConfig, prot
 
   def minimumPermittedTaxYear: TaxYear = TaxYear.ending(config.getInt("minimumPermittedTaxYear"))
 
+  def studentLoansMinimumPermittedTaxYear: TaxYear = TaxYear.ending(config.getInt("studentLoansMinimumPermittedTaxYear"))
+
   def release6DownstreamConfig: DownstreamConfig =
     downstreamConfig("release6")
 
