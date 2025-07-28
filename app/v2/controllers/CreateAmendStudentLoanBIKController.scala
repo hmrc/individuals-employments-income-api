428b465f7c853883e8ea9325316e362d0b50c76e
TwinkleLahariyaHMRC <211375592+TwinkleLahariyaHMRC@users.noreply.github.com>
2025-07-28 09:49:50 +0100

    [MTDSA-30835]: Validation and Controller changes for Create Amend Student Loan BIK endpoint

---
+++ app/v2/controllers/CreateAmendStudentLoanBIKController.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.controllers
+
+import play.api.libs.json.JsValue
+import play.api.mvc.{Action, ControllerComponents}
+import shared.config.SharedAppConfig
+import shared.controllers._
+import shared.routing.Version
+import shared.services.{AuditService, EnrolmentsAuthService, MtdIdLookupService}
+import shared.utils.IdGenerator
+import v2.controllers.validators.CreateAmendStudentLoanBIKValidatorFactory
+import v2.services.CreateAmendStudentLoansBIKService
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class CreateAmendStudentLoanBIKController @Inject() (val authService: EnrolmentsAuthService,
+                                                     val lookupService: MtdIdLookupService,
+                                                     validatorFactory: CreateAmendStudentLoanBIKValidatorFactory,
+                                                     service: CreateAmendStudentLoansBIKService,
+                                                     auditService: AuditService,
+                                                     cc: ControllerComponents,
+                                                     val idGenerator: IdGenerator)(implicit ec: ExecutionContext, appConfig: SharedAppConfig)
+    extends AuthorisedController(cc) {
+
+  val endpointName = "create-amend-student-loans-benefits-in-kind"
+
+  implicit val endpointLogContext: EndpointLogContext =
+    EndpointLogContext(
+      controllerName = "CreateAmendStudentLoanBIKController",
+      endpointName = "createAmendStudentLoansBenefitsInKind"
+    )
+
+  def createAmendStudentLoansBenefitsInKind(nino: String, employmentId: String, taxYear: String): Action[JsValue] =
+    authorisedAction(nino).async(parse.json) { implicit request =>
+      implicit val ctx: RequestContext = RequestContext.from(idGenerator, endpointLogContext)
+
+      val validator = validatorFactory.validator(
+        nino = nino,
+        taxYear = taxYear,
+        employmentId = employmentId,
+        body = request.body
+      )
+
+      val requestHandler = RequestHandler
+        .withValidator(validator)
+        .withService(service.createAndAmend)
+        .withAuditing(AuditHandler(
+          auditService = auditService,
+          auditType = "CreateAmendStudentLoanBenefitInKind",
+          apiVersion = Version(request),
+          transactionName = "create-amend-student-loans-benefits-in-kind",
+          params = Map("nino" -> nino, "taxYear" -> taxYear, "employmentId" -> employmentId),
+          requestBody = Some(request.body)
+        ))
+        .withNoContentResult(successStatus = NO_CONTENT)
+
+      requestHandler.handleRequest()
+    }
+
+}
---
+++ test/v2/controllers/validators/CreateAmendStudentLoanBIKValidatorSpec.scala
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.controllers.validators
+
+import common.errors.EmploymentIdFormatError
+import common.models.domain.EmploymentId
+import config.MockEmploymentsAppConfig
+import play.api.libs.json.{JsObject, JsValue, Json}
+import shared.models.domain.{Nino, TaxYear}
+import shared.models.errors._
+import shared.utils.UnitSpec
+import v2.models.request.createAmendStudentLoanBIK.{CreateAmendStudentLoanBIKRequest, CreateAmendStudentLoanBIKRequestBody}
+
+class CreateAmendStudentLoanBIKValidatorSpec extends UnitSpec with MockEmploymentsAppConfig {
+
+  private implicit val correlationId: String = "correlationId"
+  private val validNino                      = "AA123456B"
+  private val validTaxYear                   = "2024-25"
+  private val validEmploymentId              = "4557ecb5-fd32-48cc-81f5-e6acd1099f3c"
+
+  private val parsedNino         = Nino(validNino)
+  private val parsedTaxYear      = TaxYear.fromMtd(validTaxYear)
+  private val parsedEmploymentId = EmploymentId(validEmploymentId)
+
+  val validPayrolledBenefits = 20000.01
+
+  private val validRequestBodyJson: JsValue = Json.parse(s"""
+                                                            |{
+                                                            |  "payrolledBenefits": $validPayrolledBenefits
+                                                            |}
+                                                            |""".stripMargin)
+
+  trait Test {
+
+    def validate(nino: String = validNino,
+                 taxYear: String = validTaxYear,
+                 employmentId: String = validEmploymentId,
+                 body: JsValue = validRequestBodyJson): Either[ErrorWrapper, CreateAmendStudentLoanBIKRequest] =
+      new CreateAmendStudentLoanBIKValidator(nino, taxYear, employmentId, body, mockEmploymentsConfig)
+        .validateAndWrapResult()
+
+    def singleError(error: MtdError): Left[ErrorWrapper, Nothing] = Left(ErrorWrapper(correlationId, error))
+
+    MockedEmploymentsAppConfig.studentLoansMinimumPermittedTaxYear returns TaxYear.fromMtd("2024-25")
+  }
+
+  "validate" should {
+    "return a request object" when {
+      "valid" in new Test {
+        validate() shouldBe Right(
+          CreateAmendStudentLoanBIKRequest(
+            parsedNino,
+            parsedTaxYear,
+            parsedEmploymentId,
+            CreateAmendStudentLoanBIKRequestBody(validPayrolledBenefits)))
+      }
+    }
+
+    "return NinoFormatError error" when {
+      "an invalid nino is supplied" in new Test {
+        validate(nino = "BAD_NINO") shouldBe singleError(NinoFormatError)
+      }
+    }
+
+    "return TaxYearFormatError error" when {
+      "an invalid tax year is supplied" in new Test {
+        validate(taxYear = "BAD_TAX_YEAR") shouldBe singleError(TaxYearFormatError)
+      }
+    }
+
+    "return RuleTaxYearRangeInvalidError error" when {
+      "an invalid tax year range is supplied" in new Test {
+        validate(taxYear = "2024-26") shouldBe singleError(RuleTaxYearRangeInvalidError)
+      }
+    }
+
+    "return RuleTaxYearNotSupportedError error" when {
+      "an unsupported tax year is supplied" in new Test {
+        validate(taxYear = "2023-24") shouldBe singleError(RuleTaxYearNotSupportedError)
+      }
+    }
+
+    "return EmploymentIdFormatError error" when {
+      "an unsupported tax year is supplied" in new Test {
+        validate(employmentId = "BAD_EMP_ID") shouldBe singleError(EmploymentIdFormatError)
+      }
+    }
+
+    "return RuleIncorrectOrEmptyBodyError error" when {
+      "an empty JSON body is submitted" in new Test {
+        validate(body = JsObject.empty) shouldBe singleError(RuleIncorrectOrEmptyBodyError)
+      }
+
+      "a non-empty JSON body is submitted without any expected fields" in new Test {
+        validate(body = Json.parse("""{"field": "value"}""")) shouldBe singleError(RuleIncorrectOrEmptyBodyError.withPath("/payrolledBenefits"))
+      }
+
+      "the submitted request body where fields have incorrect type" in new Test {
+        validate(body = Json.parse("""{ "payrolledBenefits": true }""")) shouldBe singleError(RuleIncorrectOrEmptyBodyError.withPath("/payrolledBenefits"))
+      }
+    }
+
+    "return payrolledBenefitsFormatError" when {
+      "passed out of range payrolledBenefits" in new Test {
+        validate(body = Json.parse("""{ "payrolledBenefits": -100.99 }""")) shouldBe singleError(ValueFormatError.withPath("/payrolledBenefits"))
+      }
+    }
+
+    "return multiple errors" when {
+      "request supplied has multiple errors" in new Test {
+        validate("BAD_NINO", "BAD_TAX_YEAR") shouldBe
+          Left(ErrorWrapper(correlationId, BadRequestError, Some(Seq(NinoFormatError, TaxYearFormatError))))
+      }
+    }
+  }
+
+}
