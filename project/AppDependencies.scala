000612eb52c33280007e23573d92c3ac131bf865
omotrium <omo4adex@gmail.com>
2026-04-01 16:43:25 +0100

    MTDSA-33793: Improve JSON parsing and error handling in Individuals Employments Income API response processing and update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapPlayVersion = "10.6.0"
+  val bootstrapPlayVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = List(
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
@@ -13,13 +13,13 @@ object AppDependencies {
 
   val test: Seq[sbt.ModuleID] = List(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapPlayVersion,
-    "org.scalatestplus"      %% "scalacheck-1-18"        % "3.2.19.0",
+    "org.scalatestplus"      %% "scalacheck-1-19"        % "3.2.19.0",
     "org.scalamock"          %% "scalamock"              % "7.5.5"
   ).map(_ % Test)
 
   val itDependencies: Seq[ModuleID] = List(
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.21.1",
-    "io.swagger.parser.v3"         % "swagger-parser-v3"     % "2.1.38"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.21.2",
+    "io.swagger.parser.v3"         % "swagger-parser-v3"     % "2.1.39"
   ).map(_ % Test)
 
 }
