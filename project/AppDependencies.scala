b642ae9e37cd86d49d1a20b47714f03463ed175e
benryan1790 <37295698+benryan1790@users.noreply.github.com>
2026-06-02 16:19:34 +0100

    MTDSA-34584: dpendency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
     "org.typelevel"                %% "cats-core"                 % "2.13.0",
     "com.neovisionaries"            % "nv-i18n"                   % "1.29",
-    "com.github.jknack"             % "handlebars"                % "4.3.1"
+    "com.github.jknack"             % "handlebars"                % "4.5.1"
   )
 
   val test: Seq[sbt.ModuleID] = List(
@@ -18,8 +18,8 @@ object AppDependencies {
   ).map(_ % Test)
 
   val itDependencies: Seq[ModuleID] = List(
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.21.2",
-    "io.swagger.parser.v3"         % "swagger-parser-v3"     % "2.1.39"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.21.4",
+    "io.swagger.parser.v3"         % "swagger-parser-v3"     % "2.1.43"
   ).map(_ % Test)
 
 }
