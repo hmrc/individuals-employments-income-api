5ea2cf7fe13aed8989e5522ec86454e2a826b6a5
“Taiwo-AB” <63714619+Taiwo-AB@users.noreply.github.com>
2026-06-04 17:13:56 +0100

    MTDSA-34424 Add latest for Retrieve an Employment and its Financial Detailsponse body
    Add latest for Retrieve an Employment and its Financial Details

---
+++ app/v2/models/response/retrieveFinancialDetails/DownstreamSourceEnum.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.models.response.retrieveFinancialDetails
+
+import common.models.domain.MtdSourceEnum
+import play.api.libs.json.Reads
+import shared.utils.enums.Enums
+
+enum DownstreamSourceEnum(val toMtdEnum: MtdSourceEnum) {
+  case `HMRC HELD` extends DownstreamSourceEnum(MtdSourceEnum.`hmrc-held`)
+  case CUSTOMER    extends DownstreamSourceEnum(MtdSourceEnum.user)
+  case LATEST      extends DownstreamSourceEnum(MtdSourceEnum.latest)
+}
+
+object DownstreamSourceEnum {
+  given Reads[DownstreamSourceEnum] = Enums.reads(values)
+}
